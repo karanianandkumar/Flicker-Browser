c67983f7c6a4a6a5dc5913ab865505cd2779f589
karanianandkumar <karanianandkumar@yahoo.com>
2016-05-19 23:57:55 +0530

    17.Adding RecyclerView OnClick Listeners........ Complex.. Go through it Carefully..

--- app/src/main/java/com/anandkumar/flickerbrowser/MainActivity.java
+++ app/src/main/java/com/anandkumar/flickerbrowser/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +34,18 @@ public class MainActivity extends BaseActivity {
         flickerRecyclerViewAdapter=new FlickerRecyclerViewAdapter(MainActivity.this,
                 new ArrayList<Photo>());
         mRecyclerView.setAdapter(flickerRecyclerViewAdapter);
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerViewItemClickListener(this, mRecyclerView, new RecyclerViewItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this,"Single Tap",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(MainActivity.this,"Long Press",Toast.LENGTH_SHORT).show();
+            }
+        }));
 
 
     }
